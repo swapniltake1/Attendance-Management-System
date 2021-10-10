a817c7f7da10c5c38c5231155f5f2a34a0407853
SWAPNIL TAKE <takeswapnil8@gmail.com>
2021-10-10 16:28:49 +0530

    Add files via upload

---
+++ Attendance_management.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance_management;
+
+/**
+ *
+ * @author kumar
+ */
+public class Attendance_management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Welcome nw=new Welcome();
+        nw.setVisible(true);// TODO code application logic here
+    }
+    
+}
